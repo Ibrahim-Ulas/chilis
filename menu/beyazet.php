a5c1ff11dc3ca13d658154f65700d829e8275fac
Ibrahim-Ulas <iboalpulas@hotmail.com>
2025-01-17 00:31:08 +0300

    Son

--- menu/beyazet.php
+++ menu/beyazet.php
@@ -3,19 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <style>
+        p{
+            display: inline-block;
+            margin-right: 20px;
+            vertical-align: middle;
+        }
+        img{
+           
+            width:  100px;
+            height: 100px;
+            margin-right: 10px;
+            vertical-align: middle;
+        }
+    </style>
 </head>
 <body>
+    <div style="text-align: center;">
     <h1>Beyaz Etler</h1>
     <h2>Tavuklar</h2>
-    <p>Izgara Tavuk</p>
-    <p>Kızarmış Tavuk</p>
-    <p>Ballı Hardallı Tavuk</p>
-    <p>Barbekü Soslu Tavuk</p>
+    <p>Izgara Tavuk <img src="resim/ızgaratavuk.jpg" alt=""></p>
+    <p>Kızarmış Tavuk<img src="resim/kızarmıstavuk.jpg" alt=""></p>
+    <p>Ballı Hardallı Tavuk <img src="resim/ballihardal.jpg" alt=""></p>
+    <p>Barbekü Soslu Tavuk <img src="resim/barbekusoslutavuk.jpg" alt=""></p>
     <h2>Balıklar</h2>
-    <p>Hamsi</p>
-    <p>Çinekop</p>
-    <p>Levrek</p>
-    <p>Kalkan</p>
+    <p>Hamsi <img src="resim/hamsi.jpg" alt=""></p>
+    <p>Çinekop <img src="resim/cinekop.jpg" alt=""></p>
+    <p>Levrek <img src="resim/levrek.jpg" alt=""></p>
+    <p>Kalkan <img src="resim/kalkan.jpg" alt=""></p>
+    </div>
 </body>
 </html>
